3cc7f34f8aaa74a61804631945fd3a5f1630083c
AleksNik404 <107319500+AleksNik404@users.noreply.github.com>
2022-07-19 23:17:15 +0300

    simple click event

--- Study-Projects/HTML-CSS-JS/2. Jonas.io/3. JS/1. DOM_Lesson_Guess-My-Number/script.js
+++ Study-Projects/HTML-CSS-JS/2. Jonas.io/3. JS/1. DOM_Lesson_Guess-My-Number/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'Ololo';
+
+// document.querySelector('.number').textContent = '&';
+
+// document.querySelector('.guess').value = 24;
+
+let check = document.querySelector('.check');
+check.addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number!';
+  }
+});
