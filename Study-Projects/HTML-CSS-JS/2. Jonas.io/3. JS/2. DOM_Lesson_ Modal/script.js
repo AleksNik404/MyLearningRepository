e9dc47cea9ebe82df3ae2adfecb39bb89be01d25
AleksNik404 <107319500+AleksNik404@users.noreply.github.com>
2022-07-20 17:42:22 +0300

    DOM add_and_delete css_class function

--- Study-Projects/HTML-CSS-JS/2. Jonas.io/3. JS/2. DOM_Lesson_ Modal/script.js
+++ Study-Projects/HTML-CSS-JS/2. Jonas.io/3. JS/2. DOM_Lesson_ Modal/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnShowModal = document.querySelectorAll('.show-modal');
+const btnCloseModal = document.querySelector('.close-modal');
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+console.log(btnShowModal);
+
+btnShowModal.forEach(key => {
+  key.addEventListener('click', openModal);
+});
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
