efecdedc84e57bd6e82f27d562f8fc1117fdfe05
AleksNik404 <107319500+AleksNik404@users.noreply.github.com>
2022-08-05 20:23:19 +0300

    added map library Leaflet.
    We have integrated a map library and our own geolocation

--- Study-Projects/HTML-CSS-JS/2. Jonas.io/3. JS/6. OOP Mapty/script.js
+++ Study-Projects/HTML-CSS-JS/2. Jonas.io/3. JS/6. OOP Mapty/script.js
@@ -19,6 +19,20 @@ if (navigator.geolocation)
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
       console.log(`https://www.google.ru/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     () => alert('Could not get your position')
   );
