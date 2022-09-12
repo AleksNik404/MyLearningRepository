0e2138e3300cf584013cf0cf8ebd768c001ca17d
AleksNik404 <AleksNik404@gmail.com>
2022-09-12 12:30:20 +0300

    NODE: feat: connect MongoDB and Mongoose

--- 1. Jonas.io/4. NodeJS/2) Natours/server.js
+++ 1. Jonas.io/4. NodeJS/2) Natours/server.js
@@ -1,6 +1,16 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB, {}).then((con) => {
+  console.log('DB connection successful!');
+});
 
 const app = require('./app');
 
