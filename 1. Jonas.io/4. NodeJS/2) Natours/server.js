aa4ed32a0b43d44b8c51877d9f935881fb1c665b
AleksNik404 <AleksNik404@gmail.com>
2022-09-14 11:26:58 +0300

    NODE: feat: add script to import and delete data in BD

--- 1. Jonas.io/4. NodeJS/2) Natours/server.js
+++ 1. Jonas.io/4. NodeJS/2) Natours/server.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
@@ -12,8 +13,6 @@ mongoose.connect(DB, {}).then(() => {
   // eslint-disable-next-line no-console
   console.log('DB connection successful!');
 });
-
-const app = require('./app');
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
