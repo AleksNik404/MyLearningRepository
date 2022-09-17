34300c8fe452cf72c35e0998223c5900250cd6be
AleksNik404 <AleksNik404@gmail.com>
2022-09-17 13:14:21 +0300

    ZTM_20: feat: finish quote generator and start infinity scrooll

--- 2. ZTM/20 mini projects/01 Quote Generator/script.js
+++ 2. ZTM/20 mini projects/01 Quote Generator/script.js
@@ -6,11 +6,25 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
 
+// Show Loading
+function showLoadSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function removeLoadSpinner() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // GET quotes From API
 function newQuote() {
+    showLoadSpinner();
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
     // Check if author field is blank and replace ot with 'Unknown'
@@ -22,10 +36,12 @@ function newQuote() {
     else quoteText.classList.remove("long-quote");
 
     quoteText.textContent = quote.text;
+    removeLoadSpinner();
 }
 
 // GET quotes From API
 async function getQuotes() {
+    showLoadSpinner();
     const apiUrl = "https://type.fit/api/quotes";
 
     try {
@@ -36,6 +52,7 @@ async function getQuotes() {
     } catch (err) {
         console.log(err);
     }
+    removeLoadSpinner();
 }
 
 // Tweet Quote
@@ -51,3 +68,5 @@ newQuoteBtn.addEventListener("click", newQuote);
 
 // On Load
 getQuotes();
+// complete();
+// loading();
