4b90da7b37146631de9de16fa3627c224caa4a7e
AleksNik404 <AleksNik404@gmail.com>
2022-10-15 11:58:23 +0300

    NODE: feat: delete handler

--- 1. Jonas.io/4. NodeJS/2) Natours/routes/tourRoutes.js
+++ 1. Jonas.io/4. NodeJS/2) Natours/routes/tourRoutes.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 const authController = require('../controllers/authController');
-const reviewController = require('../controllers/reviewController');
+const reviewRouter = require('./reviewRoutes');
 
 const router = express.Router();
 
 // router.param('id', tourController.checkID);
+
+// POST /tour/id_tour242424/reviews
+// GET /tour/id_tour9924/reviews
+// GET /tour/id_tour9924/reviews/id_rev7247247
+
+// router
+//   .route('/:tourId/reviews')
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('user'),
+//     reviewController.createReview
+//   );
+
+router.use('/:tourId/reviews', reviewRouter);
 
 router
   .route('/top-5-cheap')
@@ -27,18 +41,6 @@ router
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
     tourController.deleteTour
-  );
-
-// POST /tour/id_tour242424/reviews
-// GET /tour/id_tour9924/reviews
-// GET /tour/id_tour9924/reviews/id_rev7247247
-
-router
-  .route('/:tourId/reviews')
-  .post(
-    authController.protect,
-    authController.restrictTo('user'),
-    reviewController.createReview
   );
 
 module.exports = router;
