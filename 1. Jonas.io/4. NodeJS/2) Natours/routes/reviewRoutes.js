d64a1c22b8dcdd66af3a67868815636428434e1e
AleksNik404 <AleksNik404@gmail.com>
2022-10-17 13:37:02 +0300

    NODE: feat: endpoint / missing authorization / review and user data / indexes
    - me endpoint
    - missing authentication and authorization
    - importing review and user data
    - improving read performance with Indexes

--- 1. Jonas.io/4. NodeJS/2) Natours/routes/reviewRoutes.js
+++ 1. Jonas.io/4. NodeJS/2) Natours/routes/reviewRoutes.js
@@ -4,11 +4,13 @@ const authController = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protect);
+
+// authController.protect, delete
 router
   .route('/')
   .get(reviewController.getAllReview)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview
@@ -17,7 +19,13 @@ router
 router
   .route('/:id')
   .get(reviewController.getReview)
-  .patch(reviewController.updateReview)
-  .delete(reviewController.deleteReview);
+  .patch(
+    authController.restrictTo('user', 'admin'),
+    reviewController.updateReview
+  )
+  .delete(
+    authController.restrictTo('user', 'admin'),
+    reviewController.deleteReview
+  );
 
 module.exports = router;
