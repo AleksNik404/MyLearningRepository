72c6ec52382cd09ec709105166026efdb72e894d
AleksNik404 <AleksNik404@gmail.com>
2022-10-18 13:35:51 +0300

    NODE: feat: finding tours within radius / agregation distances
    - geospatial queries finding tours within radius
    - agregation calculating distances

--- 1. Jonas.io/4. NodeJS/2) Natours/models/tourModel.js
+++ 1. Jonas.io/4. NodeJS/2) Natours/models/tourModel.js
@@ -115,6 +115,7 @@ const tourSchema = new mongoose.Schema(
 // tourSchema.index({ price: 1 });
 tourSchema.index({ price: 1, ratingsAverage: -1 });
 tourSchema.index({ slug: 1 });
+tourSchema.index({ startLocation: '2dsphere' });
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
@@ -173,13 +174,12 @@ tourSchema.post(/^find/, (docs, next) => {
   next();
 });
 
-// AGGREGATION MIDDLEWARE
+// // AGGREGATION MIDDLEWARE
+// tourSchema.pre('aggregate', function (next) {
+//   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
 
-tourSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-
-  next();
-});
+//   next();
+// });
 
 const Tour = mongoose.model('Tour', tourSchema);
 
