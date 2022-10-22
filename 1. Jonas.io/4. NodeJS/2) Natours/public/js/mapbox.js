4021edc0843505eabd0cbe2130d8a2624745587d
AleksNik404 <AleksNik404@gmail.com>
2022-10-22 13:08:27 +0300

    NODE: feat: mapbox and login form

--- 1. Jonas.io/4. NodeJS/2) Natours/public/js/mapbox.js
+++ 1. Jonas.io/4. NodeJS/2) Natours/public/js/mapbox.js
@@ -1,4 +1,57 @@
 /* eslint-disable */
 
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
-console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiZnJlZXNvbG8yMiIsImEiOiJjbDlqb29vcHoweTAxM25yN2RqOGR6MjI0In0.jV8WuKdCtvDSVz4hfahTKQ';
+
+const map = new mapboxgl.Map({
+  container: 'map', // container ID
+  style: 'mapbox://styles/mapbox/streets-v11', // style URL
+  scrollZoom: false,
+  center: [-74.5, 40], // starting position [lng, lat]
+  zoom: 9, // starting zoom
+  projection: 'globe', // display the map as a 3D globe
+});
+
+map.on('style.load', () => {
+  map.setFog({}); // Set the default atmosphere style
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  // Create marker
+
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  // Add marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add popup
+
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  // Extend map bounds to include current location
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});
