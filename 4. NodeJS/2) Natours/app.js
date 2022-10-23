91e3463b58068f504f334c1324c17b0380fd161e
AleksNik404 <AleksNik404@gmail.com>
2022-10-23 12:51:29 +0300

    feat: logging in users with our api - 1/3

--- 4. NodeJS/2) Natours/app.js
+++ 4. NodeJS/2) Natours/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -43,11 +44,8 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 // Body parser, reading dat`a from body into rq.body
-app.use(
-  express.json({
-    limit: '10kb',
-  })
-); // middleware
+app.use(express.json()); // middleware
+app.use(cookieParser());
 
 // Data sanitization agains NoSQL query injection
 app.use(mongoSanitize());
