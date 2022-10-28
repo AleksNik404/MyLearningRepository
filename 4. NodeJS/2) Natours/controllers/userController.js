4bc19af68c7535c442ae1298c52392af3c2ed3a8
AleksNik404 <AleksNik404@gmail.com>
2022-10-28 12:53:08 +0300

    NODE: feat: upload multiple images middleware multer/sharp

--- 4. NodeJS/2) Natours/controllers/userController.js
+++ 4. NodeJS/2) Natours/controllers/userController.js
@@ -29,12 +29,12 @@ const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
 
 exports.uploadUserPhoto = upload.single('photo');
 
-exports.resizeUserPhoto = (req, res, next) => {
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
   if (!req.file) return next();
 
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
 
-  sharp(req.file.buffer)
+  await sharp(req.file.buffer)
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({
@@ -43,7 +43,7 @@ exports.resizeUserPhoto = (req, res, next) => {
     .toFile(`public/img/users/${req.file.filename}`);
 
   next();
-};
+});
 
 const filterObj = (obj, ...alloweFields) => {
   const newObj = {};
--- 4. NodeJS/2) Natours/routes/tourRoutes.js
+++ 4. NodeJS/2) Natours/routes/tourRoutes.js
@@ -55,8 +55,14 @@ router
   .patch(
     authController.protect,
     authController.restrictTo('admin', 'lead-guide'),
+    tourController.uploadTourImages,
+    tourController.resizeTourImages,
     tourController.updateTour
   )
-  .delete(tourController.deleteTour);
+  .delete(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.deleteTour
+  );
 
 module.exports = router;
