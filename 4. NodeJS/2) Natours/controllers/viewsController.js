79899fe9efe8aeb724a0bd60f88548c10a18893c
AleksNik404 <AleksNik404@gmail.com>
2022-11-01 13:42:04 +0300

    NODE: feat:  bookings api

--- 4. NodeJS/2) Natours/controllers/viewsController.js
+++ 4. NodeJS/2) Natours/controllers/viewsController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const User = require('../models/userModel');
+const Booking = require('../models/bookingModel');
 const AppError = require('../utils/appError');
 
 const catchAsync = require('../utils/catchAsync');
@@ -43,6 +44,21 @@ exports.getAccount = catchAsync(async (req, res) => {
   res.status(200).render('account', {
     title: 'Your account',
   });
+});
+
+exports.getMyTours = catchAsync(async (req, res, next) => {
+  // 1) Find all bookings
+  const booking = await Booking.find({ user: req.user.id });
+
+  // 2) Find tours with the returned IDs
+  const tourIDs = booking.map((el) => el.tour);
+  const tours = await Tour.find({ _id: { $in: tourIDs } });
+
+  res.status(200).render('overview', {
+    title: 'My Tours',
+    tours,
+  });
+  // 3)
 });
 
 exports.updateUserData = catchAsync(async (req, res, next) => {
